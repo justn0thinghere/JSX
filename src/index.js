3f694220774f343ef68bc15875e1dc029d492be0
justn0thinghere <149658740+justn0thinghere@users.noreply.github.com>
2025-08-07 22:20:37 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>Hi Hi hi hi</p>
+  </div>,
+  document.getElementById("root")
+); //only takes 1 element, can compact together in a div
